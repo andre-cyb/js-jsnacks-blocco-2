b9367a7b9051f24e82ef30249e4d19216118e901
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-15 16:40:25 +0200

    eccolo

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let numeroUtente = prompt("inserisci un numero");
+console.log("Il numero inserito è: " + numeroUtente);
+
+if (numeroUtente % 2 === 0) {
+    console.log(numeroUtente);
+} else if (numeroUtente % 3 === 0) {
+    console.log(numeroUtente++);
+}
+
+
